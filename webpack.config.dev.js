6370869452c61c28503849de2fc47789c5d7a2d2
Stephen Brown <ontheheap@gmail.com>
2016-12-13 19:56:26 +0000

    in progress adding express and webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/app')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'app.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
