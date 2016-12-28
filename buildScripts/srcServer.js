f981cc59629e667b70c4742c98379a8c93cc6dec
stephen brown <stephen@stephens-MacBook-Air.local>
2016-12-27 20:14:52 -0700

    sane defaults for host/port for node/express

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -11,11 +11,13 @@ app.use(require('webpack-dev-middleware')(compiler, {
     publicPath: config.output.publicPath
 }));
 
-const port = process.env.PORT;
-const host = process.env.IP;
+const defaultPort = 3000;
+const defaultHost = '127.0.0.1';
+const port = process.env.PORT || defaultPort;
+const host = process.env.IP || defaultHost;
 
 app.get('/', function(req, res) {
-   res.sendFile(path.join(__dirname, '../src/index.html')); 
+   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, host, function(err) {
