554e956064f28bd398c61adb3063b34ed17abc54
stephen brown <stephen@stephens-MacBook-Air.local>
2016-12-27 20:43:24 -0700

    very small refactor of srcServer.js

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,26 +1,23 @@
-import express from 'express';
 import path from 'path';
+import express from 'express';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
 const app = express();
 const compiler = webpack(config);
+const defaultPort = 3000;
+const defaultHost = '127.0.0.1';
 
 app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
 }));
 
-const defaultPort = 3000;
-const defaultHost = '127.0.0.1';
-const port = process.env.PORT || defaultPort;
-const host = process.env.IP || defaultHost;
-
 app.get('/', function(req, res) {
    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, host, function(err) {
+app.listen(process.env.PORT || defaultPort, process.env.IP || defaultHost, function(err) {
     if(err) {
         console.log(err);
     }
