1811c99fc9360b72a45bb4ba622732c06547070a
stephen brown <ontheheap@gmail.com>
2017-01-03 18:56:13 -0700

    switch to using heroku for production api

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -14,16 +14,6 @@ app.get('/', function(req, res) {
    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function(req, res) {
-  res.json([
-    {id:1,firstName:"Stephen",lastName:"Brown",email:"ontheheap@gmail.com"},
-    {id:2,firstName:"Kelly",lastName:"Brown",email:"kelly.m.brown@gmail.com"},
-    {id:3,firstName:"Roxy",lastName:"Brown",email:"ontheheap@gmail.com"},
-    {id:4,firstName:"Lily",lastName:"Brown",email:"ontheheap@gmail.com"},
-    {id:5,firstName:"Daisy",lastName:"Brown",email:"ontheheap@gmail.com"},
-  ]);
-});
-
 app.listen(process.env.PORT || defaultPort, process.env.IP || defaultHost, function(err) {
     if(err) {
         console.log(err);
